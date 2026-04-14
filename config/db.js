bd1dd45264fa450959042b9dc2cbb295e2c5db8a
Brijesh <bkc.vns@gmail.com>
2026-04-14 11:02:03 +0530

    First Commit

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("DB Connected Successfully");
+    }
+    catch(e){
+        console.log("Unable to Connect DB",e.messge);
+    }
+ };
+module.exports = connectDB; 
---
+++ controllers/authController.js
@@ -0,0 +1,95 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All Fields are Required"
+            });
+        }
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(401).json({
+                success: false,
+                message: "User already Exists"
+            });
+        }
+
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name, email, password: hashPassword
+        });
+
+        res.status(201).json({
+            success: true,
+            message: "User Registered Successfully",
+            data: user
+        });
+    }
+    catch (e) {
+        res.status(500).json({
+            success: false,
+            message: "Unable to Register",
+            error: e.message
+        });
+    }
+};
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Email & Password are required"
+            });
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Email ID / Credentials"
+            });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid Password / Credentials"
+            });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+        res.status(200).json({
+            success: true,
+            message: "Login Successfull",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+    }
+    catch (e) {
+        res.status(500).json({
+            success: false,
+            message: "Unable to Login",
+            error: e.message
+        });
+    }
+};
+
+
+
+
+
+module.exports = { registerUser, loginUser };
